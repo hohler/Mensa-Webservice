e9d57ce4794a57f23adc854d670fbdcc956d134d
= <a.rueedlinger@gmail.com>
2013-10-16 22:27:16 +0200

    add try and catch block around the app! reason: i really dislike php why: some exceptions reveal to much information! lesson learned: do not trust pdoexception!

--- index.php
+++ index.php
@@ -27,72 +27,75 @@ $autoload = function ($className){
 
 spl_autoload_register($autoload);
 
+try {
+	$slim = new \Slim\Slim();
+	$dataSource = DataSource::createInstance($config); // create ds instance
+	$request = $slim->request();
+	$response = $slim->response();
 
-$slim = new \Slim\Slim();
-$dataSource = DataSource::createInstance($config); // create ds instance
-$request = $slim->request();
-$response = $slim->response();
+	$jsonRender = new JSONRender($request,$response);
+	$renders = array( '' => $jsonRender, '.json' => $jsonRender); // url endings .json, .xml or blank
+	// note: blank ending -> json render!
 
-$jsonRender = new JSONRender($request,$response);
-$renders = array( '' => $jsonRender, '.json' => $jsonRender); // url endings .json, .xml or blank
-// note: blank ending -> json render!
-
-// map each route with an anonymous function that binds the controller 
-// methods in the slim framework.
-foreach($routes as $route){
-	$handler = $route['handler'];
-	$controllerName = $route['controller'];
-	$path = $route['path'];
-	
-	// create an instance of $controllerName and inject some depedencies
-	$controller = new $controllerName($config,$pullTokens,$dataSource,$request,$response);
-	
-	// register each route for different renders
-	foreach($renders as $format=>$render){
-		// anonymous function
-		$func = function () use ($controller,$handler,$path,$render) {
+	// map each route with an anonymous function that binds the controller 
+	// methods in the slim framework.
+	foreach($routes as $route){
+		$handler = $route['handler'];
+		$controllerName = $route['controller'];
+		$path = $route['path'];
+		
+		// create an instance of $controllerName and inject some depedencies
+		$controller = new $controllerName($config,$pullTokens,$dataSource,$request,$response);
+		
+		// register each route for different renders
+		foreach($renders as $format=>$render){
+			// anonymous function
+			$func = function () use ($controller,$handler,$path,$render) {
+				
+				// create named parameters according the routes.php file
+				$arguments = func_get_args();
+				$paramNames = Helper::getParamNames($path);
+				
+				// check that both array have the same length and that its length is greater than 0!
+				if(count($paramNames)==count($arguments) && count($paramNames)!=0){
+					// make an associative array.
+					$params = array_combine($paramNames,$arguments);
+				} else { // otherwise just use the array
+					$params = $arguments;
+				}
+				
+				// token validation
+				if($controller->checkToken()){
+					//call the controller method, pass the $params array
+					//and render the returned response object
+					$render->render($controller->$handler($params));
+				} else {
+					$errorResponse = new Response(array(),400);
+					$errorResponse->setReason('Invalid token!');
+					$render->render($errorResponse);
+				}
+			};
 			
-			// create named parameters according the routes.php file
-			$arguments = func_get_args();
-			$paramNames = Helper::getParamNames($path);
-			
-			// check that both array have the same length and that its length is greater than 0!
-			if(count($paramNames)==count($arguments) && count($paramNames)!=0){
-				// make an associative array.
-				$params = array_combine($paramNames,$arguments);
-			} else { // otherwise just use the array
-				$params = $arguments;
-			}
-			
-			// token validation
-			if($controller->checkToken()){
-				//call the controller method, pass the $params array
-				//and render the returned response object
-				$render->render($controller->$handler($params));
+			// map route with a handler and set http methods POST,GET,PUT etc.
+			$map = $slim->map($route['path'].$format,$func); //append format to the url ending!
+			if(is_array($route['method'])){	
+				foreach($route['method'] as $m){
+					$map->via($m);
+				}
 			} else {
-				$errorResponse = new Response(array(),400);
-				$errorResponse->setReason('Invalid token!');
-				$render->render($errorResponse);
+				$map->via($route['method']);
 			}
-		};
-		
-		// map route with a handler and set http methods POST,GET,PUT etc.
-		$map = $slim->map($route['path'].$format,$func); //append format to the url ending!
-		if(is_array($route['method'])){	
-			foreach($route['method'] as $m){
-				$map->via($m);
+			
+			// set conditions
+			if(array_key_exists('conditions',$route) && is_array($route['conditions'])){
+				$map->conditions($route['conditions']);
 			}
-		} else {
-			$map->via($route['method']);
-		}
-		
-		// set conditions
-		if(array_key_exists('conditions',$route) && is_array($route['conditions'])){
-			$map->conditions($route['conditions']);
 		}
 	}
-}
 
-$slim->run ();
+	$slim->run ();
+}catch(Exception $e){
+	die("Sorry, the service is temporary unavailable.");
+}
 
 ?>
